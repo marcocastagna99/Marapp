40661452df157f868361a74f52e809a2b71f3405
enriicola <enrico.pezzano@outlook.it>
2024-12-02 11:04:30 +0100

    restored registration GUI and logic

--- lib/views/registration_view.dart
+++ lib/views/registration_view.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:marapp/providers/auth_provider.dart';
@@ -19,7 +22,26 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
   final TextEditingController _addressController = TextEditingController();
+  
   bool _isLoading = false;
+
+  String name = '';
+  String email = '';
+  String phoneNumber = '';
+  String address = '';
+  String password = '';
+  String confirmPassword = '';
+
+  bool _emailValid = true;
+  bool _phoneValid = true;
+  bool _passwordValid = true;
+  bool _passwordVisible = false;
+  bool _confirmPasswordVisible = false;
+  bool _showConfirmPassword = false;
+  bool _passwordsMatch = true;
+
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
 
   void _register(BuildContext context) async {
     if (_formKey.currentState!.validate()) {
@@ -34,17 +56,14 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
           _nameController.text.trim(),
           _phoneController.text.trim(),
           _addressController.text.trim(),
-
         );
-        // Naviga alla schermata successiva o mostra un messaggio di successo
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Registrazione completata!')),
         );
         Navigator.pushReplacement(
           context,
-          MaterialPageRoute(builder: (context) => HomeScreen()), // Aggiungi il nome della tua schermata Home
+          MaterialPageRoute(builder: (context) => HomeScreen()),
         );
-
       } catch (error) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Errore: ${error.toString()}')),
@@ -71,96 +90,155 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                TextFormField(
+                _buildRoundedTextField( // Name
                   controller: _nameController,
-                  decoration: InputDecoration(labelText: 'Nome Completo'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Inserisci il tuo nome';
-                    }
-                    return null;
-                  },
+                  label: 'Nome Completo',
+                  onChanged: (value) => name = value,
+                  errorText: _formKey.currentState?.validate() == false &&
+                          _nameController.text.isEmpty
+                      ? 'Inserisci il tuo nome'
+                      : null,
                 ),
-                TextFormField(
+                SizedBox(height: 10),
+
+                _buildRoundedTextField( // Phone Number
                   controller: _phoneController,
-                  decoration: InputDecoration(labelText: 'Numero di Telefono'),
-                  keyboardType: TextInputType.phone,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Inserisci il tuo numero di telefono';
-                    }
-                    if (!RegExp(r'^\d{10}$').hasMatch(value)) {
-                      return 'Numero non valido (10 cifre richieste)';
-                    }
-                    return null;
+                  label: 'Numero di Telefono',
+                  onChanged: (value) {
+                    setState(() {
+                      phoneNumber = value;
+                      _phoneValid = RegExp(r'^\d{10}$')
+                          .hasMatch(_phoneController.text);
+                    });
                   },
+                  errorText: _formKey.currentState?.validate() == false &&
+                          (_phoneController.text.isEmpty || !_phoneValid)
+                      ? 'Inserisci un numero di telefono valido'
+                      : null,
+                  borderColor: _phoneController.text.isEmpty
+                      ? Colors.grey 
+                      : RegExp(r'^\d{10}$').hasMatch(_phoneController.text)
+                          ? Colors.green 
+                          : Colors.red, 
                 ),
-                TextFormField(
+                SizedBox(height: 10),
+
+                _buildRoundedTextField( // Address
                   controller: _addressController,
-                  decoration: InputDecoration(labelText: 'Indirizzo'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Inserisci il tuo indirizzo';
-                    }
-                    return null;
-                  },
+                  label: 'Indirizzo',
+                  onChanged: (value) {},
+                  errorText: _formKey.currentState?.validate() == false &&
+                          _addressController.text.isEmpty
+                      ? 'Inserisci il tuo indirizzo'
+                      : null,
                 ),
-                TextFormField(
+                SizedBox(height: 10),
+
+                _buildRoundedTextField( // Email
                   controller: _emailController,
-                  decoration: InputDecoration(labelText: 'Email'),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Inserisci un\'email valida';
-                    }
-                    if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-                      return 'Formato email non valido';
-                    }
-                    return null;
+                  focusNode: _emailFocusNode,
+                  label: 'Email',
+                  onChanged: (value) {
+                    setState(() {
+                      email = value;
+                      _emailValid = RegExp(r'^[^@]+@[^@]+\.[^@]+')
+                          .hasMatch(_emailController.text);
+                    });
                   },
+                  errorText: _formKey.currentState?.validate() == false &&
+                          (_emailController.text.isEmpty || !_emailValid)
+                      ? 'Inserisci un\'email valida'
+                      : null,
+                  borderColor: _emailController.text.isEmpty
+                      ? Colors.grey 
+                      : _emailValid
+                          ? Colors.green 
+                          : Colors.red, 
                 ),
-                TextFormField(
+                SizedBox(height: 10),
+
+                // Password
+                _buildRoundedTextField(
                   controller: _passwordController,
-                  decoration: InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Inserisci una password';
-                    }
-                    if (value.length < 6) {
-                      return 'La password deve essere lunga almeno 6 caratteri';
-                    }
-                    return null;
+                  focusNode: _passwordFocusNode,
+                  label: 'Password',
+                  errorText: !_passwordValid
+                      ? 'Password must be at least 6 characters'
+                      : null,
+                  obscureText: !_passwordVisible,
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _passwordVisible
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    },
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      password = value;
+                      _passwordValid = password.length >= 6;
+                      _showConfirmPassword = password.isNotEmpty;
+                      _checkPasswordMatch();
+                    });
                   },
+                  borderColor: _passwordController.text.isEmpty
+                      ? Colors.grey // Neutral color when empty
+                      : _passwordsMatch
+                          ? Colors.green // Change to green if valid
+                          : Colors.red, // Change to red if invalid
                 ),
-                TextFormField(
-                  controller: _confirmPasswordController,
-                  decoration: InputDecoration(labelText: 'Conferma Password'),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Conferma la password';
-                    }
-                    if (value != _passwordController.text) {
-                      return 'Le password non corrispondono';
-                    }
-                    return null;
-                  },
-                ),
+                SizedBox(height: 10),
+
+                // Confirm Password
+                if (_showConfirmPassword)
+                  _buildRoundedTextField(
+                    controller: _confirmPasswordController,
+                    label: 'Confirm Password',
+                    errorText:
+                        !_passwordsMatch ? 'Passwords do not match' : null,
+                    obscureText: !_confirmPasswordVisible,
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _confirmPasswordVisible
+                            ? Icons.visibility_off
+                            : Icons.visibility,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _confirmPasswordVisible = !_confirmPasswordVisible;
+                        });
+                      },
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        confirmPassword = value;
+                        _checkPasswordMatch();
+                      });
+                    },
+                    borderColor: _passwordsMatch
+                        ? Colors.green
+                        : null, // Same for confirm password field
+                  ),
+                if (_showConfirmPassword) SizedBox(height: 10),
+
                 SizedBox(height: 20),
                 _isLoading
                     ? Center(child: CircularProgressIndicator())
                     : ElevatedButton(
-                  onPressed: () => _register(context),
-                  child: Text('Registrati'),
-                ),
+                        onPressed: () => _register(context),
+                        child: Text('Registrati'),
+                      ),
                 SizedBox(height: 20),
-                // Link per la pagina di login
                 TextButton(
                   onPressed: () {
                     Navigator.pushReplacement(
                       context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()), // Cambia con la tua LoginScreen
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
                     );
                   },
                   child: Text(
@@ -189,4 +267,128 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
     _addressController.dispose();
     super.dispose();
   }
+
+  Widget _buildRoundedTextField({
+    required TextEditingController controller,
+    required String label,
+    FocusNode? focusNode,
+    bool obscureText = false,
+    String? errorText,
+    Widget? suffixIcon,
+    required Function(String) onChanged,
+    bool enabled = true,
+    Color? borderColor,
+  }) {
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    final textColor = isDarkMode ? Colors.white : Colors.black;
+
+    if (Platform.isIOS || Platform.isMacOS) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CupertinoTextField(
+            controller: controller,
+            focusNode: focusNode,
+            obscureText: obscureText,
+            enabled: enabled,
+            placeholder: label,
+            suffix: suffixIcon,
+            onChanged: onChanged,
+            style: TextStyle(color: textColor),
+            decoration: BoxDecoration(
+              border: Border.all(color: borderColor ?? Colors.grey),
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          ),
+          if (errorText != null)
+            Padding(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: Text(
+                errorText,
+                style: TextStyle(color: Colors.red, fontSize: 12),
+              ),
+            ),
+        ],
+      );
+    } else {
+      return Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: borderColor ?? Colors.grey),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: TextField(
+          controller: controller,
+          focusNode: focusNode,
+          obscureText: obscureText,
+          enabled: enabled,
+          decoration: InputDecoration(
+            labelText: label,
+            errorText: errorText,
+            border: InputBorder.none,
+            contentPadding:
+                EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            suffixIcon: suffixIcon,
+          ),
+          style: TextStyle(color: textColor),
+          onChanged: onChanged,
+        ),
+      );
+    }
+  }
+
+
+void _checkPasswordMatch() {
+    setState(() {
+      _passwordsMatch =
+          _passwordController.text == _confirmPasswordController.text;
+    });
+  }
+
+  // TODO check if unused
+  // void _completeRegistration() async {
+  //   final authProvider = Provider.of<AuthProvider>(context, listen: false);
+  //   final email = _emailController.text;
+  //   final password = _passwordController.text;
+
+  //   if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(email)) {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(content: Text('Invalid email format')),
+  //     );
+  //     return;
+  //   }
+
+  //   if (password.isEmpty || password.length < 6) {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(content: Text('Password must be at least 6 characters long')),
+  //     );
+  //     return;
+  //   }
+
+  //   if (password != _confirmPasswordController.text) {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(content: Text('Passwords do not match')),
+  //     );
+  //     return;
+  //   }
+
+  //   final success = await authProvider.signUp(
+  //       email,
+  //       password,
+  //       _nameController.text,
+  //       _phoneController.text,
+  //       _addressController.text);
+
+  //   if (success) {
+  //     Navigator.pushReplacement(
+  //       context,
+  //       MaterialPageRoute(builder: (context) => HomeScreen()),
+  //     );
+  //   } else {
+  //     ScaffoldMessenger.of(context).showSnackBar(
+  //       SnackBar(content: Text('Registration failed')),
+  //     );
+  //   }
+  // }
+  
 }
